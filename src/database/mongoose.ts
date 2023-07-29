9e499f94143356eb6a9a069806509a05ff426070
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-07-29 08:47:38 +0100

    change

--- src/database/mongoose.ts
+++ src/database/mongoose.ts
@@ -1,17 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import * as dotenv from 'dotenv';
-
+import { AppController } from '../app.controller';
+import { AppService } from '../app.service';
 dotenv.config();
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(process.env.MONGODB_URL, {
-      useFindAndModify: false,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    }),
-  ],
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URI)],
+  controllers: [AppController],
+  providers: [AppService],
 })
+
 export class MongoModule {}
